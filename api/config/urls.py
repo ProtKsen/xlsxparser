934a356080619cfa1b405013c4ef735a14db4322
ProtKen <protcenko.kseniya@gmail.com>
2023-07-27 10:23:11 +0500

    ref: change url for uploading

--- api/config/urls.py
+++ api/config/urls.py
@@ -4,4 +4,4 @@ URL configuration for src project.
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("file/", include("xlsx_uploader.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("xlsx_uploader.urls"))]
