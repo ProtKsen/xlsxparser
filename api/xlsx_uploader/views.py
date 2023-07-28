932e12a9e0b2fe5f42eb748c2aee3cd5873ecefb
ProtKen <protcenko.kseniya@gmail.com>
2023-07-28 16:21:13 +0500

    feature: use redirect in upload handle

--- api/xlsx_uploader/views.py
+++ api/xlsx_uploader/views.py
@@ -4,7 +4,7 @@ from botocore.exceptions import ClientError, EndpointConnectionError
 from config import settings
 from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
 from pika.exceptions import AMQPError
 from xlsx_uploader.aws import upload_file
@@ -20,31 +20,31 @@ bucket = settings.AWS_BUCKET_NAME_INPUT
 def upload(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
+
         if form.is_valid():
             file = request.FILES["file"]
 
+            # check file extension
             file_extension = os.path.splitext(file.name)[-1]
             if file_extension.lower() not in ALLOWED_EXTENSIONS:
                 messages.add_message(request, messages.ERROR, "Неверный формат файла")
-                form = UploadFileForm()
-                context = {"form": form}
-                return render(request, "upload.html", context)
+                return redirect(".")
 
+            # check file is saved in storage
             try:
                 upload_file(file, bucket, file.name)
             except (ClientError, EndpointConnectionError):
                 messages.add_message(request, messages.ERROR, "Не удалось сохранить файл")
-                form = UploadFileForm()
-                return render(request, "upload.html", {"form": form})
+                return redirect(".")
 
+            # check filename is queued for parsing
             try:
                 publish(settings.PARSING_QUEUE_NAME, file.name)
             except AMQPError:
                 messages.add_message(
                     request, messages.ERROR, "Не удалось отправить файл на распознавание"
                 )
-                form = UploadFileForm()
-                return render(request, "upload.html", {"form": form})
+                return redirect(".")
 
             return HttpResponse("Файл передан в обработку", status=200)
 
